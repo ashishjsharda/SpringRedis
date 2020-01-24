64e08ca43f9c1dd3001bbfd26aea8d7ea2780c09
Ashish Sharda <ashishjsharda@gmail.com>
2020-01-24 08:41:44 -0500

    Add all my files

---
+++ src/main/java/com/ashish/cache/springredisexample/UserRepository.java
@@ -0,0 +1,14 @@
+package com.ashish.cache.springredisexample;
+
+import com.ashish.cache.springredisexample.model.User;
+
+import java.util.List;
+import java.util.Map;
+
+public interface UserRepository {
+    void save(User user);
+    Map<String,User> findAll();
+    void update(User user);
+    void delete(String id);
+    User findById(String id);
+}
